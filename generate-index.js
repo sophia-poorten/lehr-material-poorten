0e6d3b76fcbeb5cfba4b8d80938ec69647f636b6
jonathan-grumke <grumkejonathan@yahoo.de>
2025-08-12 20:36:41 +0200

    loop directories iteratively

--- generate-index.js
+++ generate-index.js
@@ -1,40 +1,66 @@
-// generate-index.js
 const fs = require('fs');
 const path = require('path');
 
-const docsFolder = './docs';
-const files = fs.readdirSync(docsFolder).filter(f => f.endsWith('.pdf'));
+const rootFolder = './docs';
 
-let head = `
+function getAllSubdirectories(dir) {
+    let subdirs = [];
+    fs.readdirSync(dir).forEach(f => {
+        const fullPath = path.join(dir, f);
+        if (fs.statSync(fullPath).isDirectory()) {
+            subdirs.push(fullPath);
+            subdirs = subdirs.concat(getAllSubdirectories(fullPath));
+        }
+    });
+    return subdirs;
+}
+
+const allDirs = [rootFolder, ...getAllSubdirectories(rootFolder)];
+
+allDirs.forEach(dir => {
+    const files = fs.readdirSync(dir).filter(f => fs.statSync(path.join(dir, f)).isFile()).filter(f => f.endsWith('.pdf'));
+    const subdirs = fs.readdirSync(dir).filter(f => fs.statSync(path.join(dir, f)).isDirectory());
+    const relDir = path.relative(rootFolder, dir) || '.';
+    let head = `
 <head>
   <meta charset="UTF-8">
-  <title>PDF Index</title>
+  <title>${relDir}</title>
 </head>
-`
-
-let body = `
+`;
+    let body = `
 <body>
-  <h1>Available PDFs</h1>
+  <a href="../index.html">zurück</a>
+  <h1>Inhalt des Ordners ${relDir}</h1>
   <ul>
 `;
 
-files.forEach(file => {
-    body += `    <li><a href="docs/${file}" target="_blank">${file}</a></li>\n`;
-});
+    // List files
+    files.forEach(file => {
+        // const filePath = path.join(relDir, file).replace(/\\/g, '/');
+        const filePath = path.join(file).replace(/\\/g, '/');
+        body += `    <li><a href="${filePath}" target="_blank">${file}</a></li>\n`;
+    });
 
-body += `
+    // List subdirectories
+    subdirs.forEach(subdir => {
+        // const subdirPath = path.join(relDir, subdir).replace(/\\/g, '/');
+        const subdirPath = path.join(subdir).replace(/\\/g, '/');
+        body += `    <li><a href="${subdirPath}/index.html">${subdir}/</a></li>\n`;
+    });
+
+    body += `
   </ul>
 </body>
 `;
 
-let html = `
+    let html = `
 <!DOCTYPE html>
 <html>
 ${head}
 ${body}
 </html>
-`
+`;
 
-
-fs.writeFileSync('index.html', html);
-console.log('index.html generated!');
+    fs.writeFileSync(path.join(dir, 'index.html'), html);
+    console.log(`index.html generated in ${dir}`);
+});
